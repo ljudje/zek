6d3a4e346d80eee5d767b0c537f1f504e3aaecd8
Srdjan Prodanovic <srdjan.prodanovic@gmail.com>
2016-03-31 20:42:20 +0200

    delete cms

--- cms/components/js/pgui.view-page-main.js
+++
@@ -1,8 +0,0 @@
-define(function(require, exports, module) {
-    $(function() {
-        var $body = $('body');
-        require(['pgui.layout'], function(instance){
-            instance.updatePopupHints($body);
-        });
-    });
-});
