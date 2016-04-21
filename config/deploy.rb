1174ba62659b9a2f61e9e6711162c13da5f5a074
Srdjan Prodanovic <srdjan.prodanovic@gmail.com>
2016-04-21 16:54:27 +0200

    Rename deployment to `static-zek`

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,5 @@
-# set :application, "zenegg-front"
-# set :repository,  "git@gitlab.com:srdjan.prodanovic/zenegg-front.git"
+set :application, "static-sek"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-#role :app, "your app-server here"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-
-# require "bundler/capistrano"
-# require 'sidekiq/capistrano'
-
-set :application, "zek-static"
-
-# set :location, "tesla.ljudje.si"
 set :user, "zek"
 server "zek.ljudje.si", :web, :app, :db, primary: true
 set :use_sudo, false
@@ -28,15 +12,9 @@ set :deploy_via, :remote_cache
 set :scm, "git"
 set :repository, "git@github.com:ljudje/zek"
 set :branch, "zekstatic"
-# puts "LOL #{File.join(current_path,'dist')}"
-# set :copy_dir, "#{File.join(current_path,'dist')}"
-# set :copy_remote_dir, "/home/#{user}/apps/#{application}/public"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-
-
-# before 'deploy:setup', 'remote:create_copy_remote_dir'
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
@@ -45,20 +23,6 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
   end
 end
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
 
 namespace :deploy do
   task :migrate do
@@ -77,34 +41,4 @@ namespace :deploy do
     puts "    not doing restart because not a Rails application."
   end
 end
-
-
-
-
-
-# set :application, "set your application name here"
-# set :repository,  "set your repository location here"
-
-# # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-# role :app, "your app-server here"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# # if you want to clean up old releases on each deploy uncomment this:
-# # after "deploy:restart", "deploy:cleanup"
-
-# # if you're still using the script/reaper helper you will need
-# # these http://github.com/rails/irs_process_scripts
-
-# # If you are using Passenger mod_rails uncomment this:
-# # namespace :deploy do
-# #   task :start do ; end
-# #   task :stop do ; end
-# #   task :restart, :roles => :app, :except => { :no_release => true } do
-# #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-# #   end
-# # end
 
